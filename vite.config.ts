fef72112798b7e268fdf7ea4ca6c29cc639850e9
MilenaMMay <milenammay@MilenaMMays-Laptop.local>
2024-04-10 11:45:20 +0200

    Correct public path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://milenammay.github.io/test-gh-pages/',
+  base: 'https://milenammay.github.io/sustainability-walk/',
   plugins: [
     vue(),
     vueJsx(),
