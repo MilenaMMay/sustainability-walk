fb8d2b660353063a39832337c447ab88be7bc3b9
MilenaMMay <milena@growing-excellence.tech>
2024-04-10 13:35:19 +0200

    Type correctly

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,33 @@ import HelloWorld from './components/HelloWorld.vue'
 
 import 'leaflet/dist/leaflet.css'
 import * as L from 'leaflet'
+import type { LatLng } from 'leaflet'
 
-const universityCologne = [50.928489708499356, 6.929532458566885]
+const universityCologne: LatLng = L.latLng(50.928489708499356, 6.929532458566885)
 const zoomLevel = 18 // maximum zoom level so that we start as close as possible
 const minZoom = 15 // avoids that users zoom out of the relevant space
 
-const sustainabilityMap = ref(null)
+//const sustainabilityMap = ref(null)
 
-const places = [
-  { name: 'Phil Café', coordinates: [50.92814086076388, 6.92777104434133] },
-  { name: 'Bistro Uni E-Raum', coordinates: [50.928439764085766, 6.929710116076665] }
+interface Place {
+  name: string
+  coordinates: LatLng
+}
+
+const places: Place[] = [
+  { name: 'Phil Café', coordinates: L.latLng(50.92814086076388, 6.92777104434133) },
+  { name: 'Bistro Uni E-Raum', coordinates: L.latLng(50.928439764085766, 6.929710116076665) }
 ]
 
 onMounted(() => {
-  sustainabilityMap.value = L.map('map').setView(universityCologne, zoomLevel)
+  var sustainabilityMap = L.map('map').setView(universityCologne, zoomLevel)
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     minZoom: minZoom
-  }).addTo(sustainabilityMap.value)
+  }).addTo(sustainabilityMap)
 
   places.forEach((place) => {
-    const marker = L.marker(place.coordinates).addTo(sustainabilityMap.value)
+    const marker = L.marker(place.coordinates).addTo(sustainabilityMap)
     marker.bindPopup(place.name)
   })
 })
