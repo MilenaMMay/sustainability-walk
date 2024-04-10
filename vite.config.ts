8027c8c4c31792842bc38ea27b15cd2fe89a36ff
MilenaMMay <milenammay@MilenaMMays-Laptop.local>
2024-04-10 12:34:57 +0200

    Change from absolute to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://milenammay.github.io/sustainability-walk/',
+  base: '/sustainability-walk/',
   plugins: [
     vue(),
     vueJsx(),
